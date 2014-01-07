e2ea1b172eee977743bde66deee3433e0a8be670
Peter Sobot <psobot@gmail.com>
2014-01-07 11:30:25 -0500

    wget instead of curl

--- setup_scala.sh
+++ setup_scala.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.1/sbt-launch.jar > sbt-launch.jar
+wget -N http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.1/sbt-launch.jar
 
 cat > sbt << EOF
 SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
