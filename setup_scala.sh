cdc54ce1d72157bf81395bb4c297a696196654c8
Peter Sobot <psobot@gmail.com>
2014-01-07 11:20:00 -0500

    Fixed setup script.

--- setup_scala.sh
+++ setup_scala.sh
@@ -1,12 +1,10 @@
-#!/bin/bash -x 
-
-set +e
+#!/bin/bash
 
 curl http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.1/sbt-launch.jar > sbt-launch.jar
 
 cat > sbt << EOF
 SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"
+java \$SBT_OPTS -jar `dirname \$0`/sbt-launch.jar "\$@"
 EOF
 
 chmod u+x sbt
