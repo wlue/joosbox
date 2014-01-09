f88d612b28d3ff2212509949361fdbd381372868
Wen-Hao Lue <me@wenhaolue.com>
2014-01-09 15:38:29 -0500

    Change sbt runner.

--- setup_scala.sh
+++ setup_scala.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-
-wget -N http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.1/sbt-launch.jar
-
-cat > sbt << EOF
-SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-java \$SBT_OPTS -jar `dirname \$0`/sbt-launch.jar "\$@"
-EOF
-
+#!/bin/bash -ex
+curl -s https://raw.github.com/paulp/sbt-extras/master/sbt > sbt
 chmod u+x sbt
 ./sbt compile
