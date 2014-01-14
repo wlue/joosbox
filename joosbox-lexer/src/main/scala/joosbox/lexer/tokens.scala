7c1f5a8a868e761c7e9c12944caae5ffc3a22947
Chris Taylor <cktaylor.se@gmail.com>
2014-01-14 17:17:27 -0500

    Regexs for all the tokens we need to lexically support.

---
+++ joosbox-lexer/src/main/scala/joosbox/lexer/tokens.scala
@@ -0,0 +1,61 @@
+import scala.util.matching.Regex
+
+val QUESTION      = """[?]""".r
+val LPAREN        = """[(]""".r
+val RPAREN        = """[)]""".r
+val LBRACK        = """[\[]""".r
+val RBRACK        = """[\]]""".r
+val LCURLY        = """[{]""".r
+val RCURLY        = """[}]""".r
+val COLON         = """[:]""".r
+val COMMA         = """[,]""".r
+val DOT           = """[.]""".r
+val ASSIGN        = """[=]""".r
+val EQUAL         = """[=][=]""".r
+val LNOT          = """[!]""".r
+val BNOT          = """[~]""".r
+val NOT_EQUAL     = """[!][=]""".r
+val DIV           = """[/]""".r
+val DIV_ASSIGN    = """[/][=]""".r
+val PLUS          = """[+]""".r
+val PLUS_ASSIGN   = """[+][=]""".r
+val INC           = """[+][+]""".r
+val MINUS         = """[-]""".r
+val MINUS_ASSIGN  = """[-][=]""".r
+val DEC           = """[-][-]""".r
+val STAR          = """[*]""".r
+val STAR_ASSIGN   = """[*][=]""".r
+val MOD           = """[%]""".r
+val MOD_ASSIGN    = """[%][=]""".r
+val SR            = """[>][>]""".r
+val SR_ASSIGN     = """[>][>][=]""".r
+val BSR           = """[>][>][>]""".r
+val BSR_ASSIGN    = """[>][>][>][=]""".r
+val GE            = """[>][=]""".r
+val GT            = """[>]""".r
+val SL            = """[<][<]""".r
+val SL_ASSIGN     = """[<][<][=]""".r
+val LE            = """[<][=]""".r
+val LT            = """[<]""".r
+val BXOR          = """[\^]""".r
+val BXOR_ASSIGN   = """[\^][=]""".r
+val BOR           = """[|]""".r
+val BOR_ASSIGN    = """[|][=]""".r
+val LOR           = """[|][|]""".r
+val BAND          = """[&]""".r
+val BAND_ASSIGN   = """[&][=]""".r
+val LAND          = """[&][&]""".r
+val SEMI          = """[;]""".r
+
+val WHITESPACE    = """\s\s*""".r
+val SL_COMMENT    = """//[^(\n|\r)]*[(\n|\r(\n)?]""".r
+val ML_COMMENT    = """/\*([^(\n|\r)]*(\r\n|\r|\n))*\*/""".r
+val JD_COMMENT    = """/\*\*(\*[^(\n|\r)]*(\r\n|\r|\n))*\*/""".r
+
+val NUM           = """\d\d*""".r
+val IDENTIFIER    = """[a-zA-Z_\$][a-zA-Z_0-9\$]*""".r
+
+val ESC_CHAR      = """\\(n|r|t|b|f|"|'|\\|[0..9])""".r
+val CHAR_LITERAL  = new Regex("\'("+ESC_CHAR+"|([^(\\n|\\r|\\\\|\')]))\'")
+val STR_LITERAL   = new Regex("\"("+ESC_CHAR+"|([^(\\n|\\r|\\\\|\")]))*\"")
+
