27b76c53ebc308be53935856fa04a039260ae892
Peter Sobot <psobot@gmail.com>
2014-01-28 12:11:24 -0500

    Token tests up to BinaryShiftRightAssign.

--- joosbox-lexer/src/test/scala/joosbox/lexer/TokenSpec.scala
+++ joosbox-lexer/src/test/scala/joosbox/lexer/TokenSpec.scala
@@ -44,5 +44,302 @@ class TokenSpec extends Specification {
         TokenNFA.nfas(Token.RightBracket).toDFA.consume("[") must beEqualTo(None)
       }
     }
+
+    "match curlies" in {
+      "leftcurly success" in {
+        TokenNFA.nfas(Token.LeftCurly).toDFA.consume("{") must beEqualTo(Some(State("{"), ""))
+      }
+      "leftcurly failure" in {
+        TokenNFA.nfas(Token.LeftCurly).toDFA.consume("(") must beEqualTo(None)
+      }
+      "rightcurly success" in {
+        TokenNFA.nfas(Token.RightCurly).toDFA.consume("}") must beEqualTo(Some(State("}"), ""))
+      }
+      "rightcurly failure" in {
+        TokenNFA.nfas(Token.RightCurly).toDFA.consume("]") must beEqualTo(None)
+      }
+    }
+
+    "match colon" in {
+      "success" in {
+        TokenNFA.nfas(Token.Colon).toDFA.consume(":") must beEqualTo(Some(State(":"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Colon).toDFA.consume(";") must beEqualTo(None)
+      }
+    }
+
+    "match comma" in {
+      "success" in {
+        TokenNFA.nfas(Token.Comma).toDFA.consume(",") must beEqualTo(Some(State(","), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Comma).toDFA.consume(";") must beEqualTo(None)
+      }
+    }
+
+    "match dot" in {
+      "success" in {
+        TokenNFA.nfas(Token.Dot).toDFA.consume(".") must beEqualTo(Some(State("."), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Dot).toDFA.consume(";") must beEqualTo(None)
+      }
+    }
+
+    "match assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.Assign).toDFA.consume("=") must beEqualTo(Some(State("="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Assign).toDFA.consume(",") must beEqualTo(None)
+      }
+    }
+
+    "match equal" in {
+      "success" in {
+        TokenNFA.nfas(Token.Equal).toDFA.consume("==") must beEqualTo(Some(State("=="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Equal).toDFA.consume("=") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Equal).toDFA.consume(",") must beEqualTo(None)
+      }
+    }
+
+    "match logical not" in {
+      "success" in {
+        TokenNFA.nfas(Token.LogicalNot).toDFA.consume("!") must beEqualTo(Some(State("!"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.LogicalNot).toDFA.consume("~") must beEqualTo(None)
+      }
+    }
+
+    "match binary not" in {
+      "success" in {
+        TokenNFA.nfas(Token.BinaryNot).toDFA.consume("~") must beEqualTo(Some(State("~"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.BinaryNot).toDFA.consume("!") must beEqualTo(None)
+      }
+    }
+
+    "match not equal" in {
+      "success" in {
+        TokenNFA.nfas(Token.NotEqual).toDFA.consume("!=") must beEqualTo(Some(State("!="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.NotEqual).toDFA.consume("!") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.NotEqual).toDFA.consume("=") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.NotEqual).toDFA.consume("==") must beEqualTo(None)
+      }
+    }
+
+    "match divide" in {
+      "success" in {
+        TokenNFA.nfas(Token.Divide).toDFA.consume("/") must beEqualTo(Some(State("/"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Divide).toDFA.consume("\\") must beEqualTo(None)
+      }
+    }
+
+    "match divide-assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.DivideAssign).toDFA.consume("/=") must beEqualTo(Some(State("/="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.DivideAssign).toDFA.consume("\\=") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.DivideAssign).toDFA.consume("/") must beEqualTo(None)
+      }
+    }
+
+    "match plus" in {
+      "success" in {
+        TokenNFA.nfas(Token.Plus).toDFA.consume("+") must beEqualTo(Some(State("+"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Plus).toDFA.consume("-") must beEqualTo(None)
+      }
+    }
+
+    "match plus-assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.PlusAssign).toDFA.consume("+=") must beEqualTo(Some(State("+="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.PlusAssign).toDFA.consume("+") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.PlusAssign).toDFA.consume("=") must beEqualTo(None)
+      }
+    }
+
+    "match increment" in {
+      "success" in {
+        TokenNFA.nfas(Token.Increment).toDFA.consume("++") must beEqualTo(Some(State("++"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Increment).toDFA.consume("+") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Increment).toDFA.consume("=") must beEqualTo(None)
+      }
+    }
+
+    "match minus" in {
+      "success" in {
+        TokenNFA.nfas(Token.Minus).toDFA.consume("-") must beEqualTo(Some(State("-"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Minus).toDFA.consume("+") must beEqualTo(None)
+      }
+    }
+
+    "match minus-assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.MinusAssign).toDFA.consume("-=") must beEqualTo(Some(State("-="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.PlusAssign).toDFA.consume("-") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.PlusAssign).toDFA.consume("=") must beEqualTo(None)
+      }
+    }
+
+    "match decrement" in {
+      "success" in {
+        TokenNFA.nfas(Token.Decrement).toDFA.consume("--") must beEqualTo(Some(State("--"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Decrement).toDFA.consume("-") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Decrement).toDFA.consume("+") must beEqualTo(None)
+      }
+    }
+
+    "match star" in {
+      "success" in {
+        TokenNFA.nfas(Token.Star).toDFA.consume("*") must beEqualTo(Some(State("*"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Star).toDFA.consume("+") must beEqualTo(None)
+      }
+    }
+
+    "match star-assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.StarAssign).toDFA.consume("*=") must beEqualTo(Some(State("*="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.StarAssign).toDFA.consume("*") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.StarAssign).toDFA.consume("=") must beEqualTo(None)
+      }
+    }
+
+    "match modulo" in {
+      "success" in {
+        TokenNFA.nfas(Token.Modulo).toDFA.consume("%") must beEqualTo(Some(State("%"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.Modulo).toDFA.consume("+") must beEqualTo(None)
+      }
+    }
+
+    "match modulo-assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.ModuloAssign).toDFA.consume("%=") must beEqualTo(Some(State("%="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.ModuloAssign).toDFA.consume("%") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.ModuloAssign).toDFA.consume("=") must beEqualTo(None)
+      }
+    }
+
+    "match greater than" in {
+      "success" in {
+        TokenNFA.nfas(Token.GreaterThan).toDFA.consume(">") must beEqualTo(Some(State(">"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.GreaterThan).toDFA.consume("<") must beEqualTo(None)
+      }
+    }
+
+    "match greater than equal" in {
+      "success" in {
+        TokenNFA.nfas(Token.GreaterEqual).toDFA.consume(">=") must beEqualTo(Some(State(">="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.GreaterEqual).toDFA.consume(">") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.GreaterEqual).toDFA.consume("=") must beEqualTo(None)
+      }
+    }
+
+    "match shift right" in {
+      "success" in {
+        TokenNFA.nfas(Token.ShiftRight).toDFA.consume(">>") must beEqualTo(Some(State(">>"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.ShiftRight).toDFA.consume(">=") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.ShiftRight).toDFA.consume(">") must beEqualTo(None)
+      }
+    }
+
+    "match shift right assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.ShiftRightAssign).toDFA.consume(">>=") must beEqualTo(Some(State(">>="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.ShiftRightAssign).toDFA.consume(">>") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.ShiftRightAssign).toDFA.consume(">=") must beEqualTo(None)
+      }
+    }
+
+    "match binary shift right" in {
+      "success" in {
+        TokenNFA.nfas(Token.BinaryShiftRight).toDFA.consume(">>>") must beEqualTo(Some(State(">>>"), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.BinaryShiftRight).toDFA.consume(">>=") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.BinaryShiftRight).toDFA.consume(">>") must beEqualTo(None)
+      }
+    }
+
+    "match binary shift right assign" in {
+      "success" in {
+        TokenNFA.nfas(Token.BinaryShiftRightAssign).toDFA.consume(">>>=") must beEqualTo(Some(State(">>>="), ""))
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.BinaryShiftRightAssign).toDFA.consume(">>>") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.BinaryShiftRightAssign).toDFA.consume(">>=") must beEqualTo(None)
+      }
+      "failure" in {
+        TokenNFA.nfas(Token.BinaryShiftRightAssign).toDFA.consume(">>") must beEqualTo(None)
+      }
+    }
   }
 }
