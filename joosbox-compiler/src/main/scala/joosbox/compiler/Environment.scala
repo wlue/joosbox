85e473425e519b02e829fdf0ad9317fac0ba0217
Wen-Hao Lue <me@wenhaolue.com>
2014-03-02 18:02:16 -0500

    Environment stuff

--- joosbox-compiler/src/main/scala/joosbox/compiler/Environment.scala
+++ joosbox-compiler/src/main/scala/joosbox/compiler/Environment.scala
@@ -11,10 +11,43 @@ import AbstractSyntaxNode.{
 
 sealed trait Environment {
   val parent: Option[Environment]
-  def lookup(name: Name): Option[Referenceable]
+
+  def lookup(name: Name): Option[Referenceable] = {
+    // Recursively look up to each parent.
+    val result = search(name)
+    result match {
+      case Some(ref) => Some(ref)
+      case None =>
+        parent.flatMap { env: Environment => env.lookup(name) }
+    }
+  }
+
+  /**
+   * Search the current scope for the name.
+   */
+  def search(name: Name): Option[Referenceable]
 }
 
+
+/**
+ * Top level environment.
+ */
 class CompilationEnvironment extends Environment {
   val parent: Option[Environment] = None
-  def lookup(name: Name): Option[Referenceable] = None
+
+  // TODO
+  def search(name: Name): Option[Referenceable] = None
+}
+
+
+/**
+ * Environment for file (Compilation Unit in AST).
+ */
+class FileEnvironment(
+  par: CompilationEnvironment
+) extends Environment {
+  val parent: Option[CompilationEnvironment] = Some(par)
+
+  // TODO
+  def search(name: Name): Option[Referenceable] = None
 }
