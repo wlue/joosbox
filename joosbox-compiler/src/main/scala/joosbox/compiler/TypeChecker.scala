7ae039a53b8fc0ff6eecf2b80281dbd5b4dd3bfd
Chris Taylor <cktaylor.se@gmail.com>
2014-03-13 17:26:54 -0400

    Resolve type stuff for logicalexpressions (currently commented)

--- joosbox-compiler/src/main/scala/joosbox/compiler/TypeChecker.scala
+++ joosbox-compiler/src/main/scala/joosbox/compiler/TypeChecker.scala
@@ -21,7 +21,7 @@ object TypeChecker {
     Map.empty
   }
 
-  def resolveType(node: AbstractSyntaxNode) : Option[Type] = {
+  def resolveType(node: AbstractSyntaxNode)(implicit mapping: EnvironmentMapping) : Option[Type] = {
     node match {
       case TrueLiteral => Some(BooleanKeyword)
       case FalseLiteral => Some(BooleanKeyword)
@@ -29,12 +29,38 @@ object TypeChecker {
       case x: StringLiteral =>
         Some(ClassType(QualifiedName("java.lang.String".split("\\.").map(InputString(_))).toTypeName))
       case c: CastExpression => Some(c.targetType)
+      case e: ExpressionName =>
+        val env = mapping.enclosingScopeOf(node).get
+        val lookup = ExpressionNameLookup(e)
+        val ref = env.lookup(lookup).get
+        ref match {
+          case p : FormalParameter => Some(p.varType)
+          case f : FieldDeclaration => Some(f.memberType)
+          case v : LocalVariableDeclaration => Some(v.memberType)
+          case v : ForVariableDeclaration => Some(v.typeDeclaration)
+          case _ => throw new SyntaxError("Could not resolve type of expression.");
+        }
       case _ => None
+    }
+  }
+
+  def checkLogicalExpression(e1: Expression, e2: Expression)(implicit mapping: EnvironmentMapping) = {
+    resolveType(e1) match {
+      case Some(BooleanKeyword) => Unit
+      case _ => throw new SyntaxError("Bitwise operators aren't supported.")
+    }
+    resolveType(e2) match {
+      case Some(BooleanKeyword) => Unit
+      case _ => throw new SyntaxError("Bitwise operators aren't supported.")
     }
   }
 
   def check(node: AbstractSyntaxNode)(implicit mapping: EnvironmentMapping) {
     node match {
+      case e: AndExpression =>
+        Unit //TypeChecker.checkLogicalExpression(e.e1, e.e2)
+      case e: OrExpression =>
+        Unit //TypeChecker.checkLogicalExpression(e.e1, e.e2)
       case c : ClassCreationPrimary =>
         val classType : ClassType = c.classType
         val env = mapping.enclosingScopeOf(node).get
