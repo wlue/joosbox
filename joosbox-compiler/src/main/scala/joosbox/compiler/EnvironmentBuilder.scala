424ce758e9f86fe155e5e4b69ca398261863bee4
Chris Taylor <cktaylor.se@gmail.com>
2014-03-13 17:36:04 -0400

    linkedScopeReferences doesn't actually break things

--- joosbox-compiler/src/main/scala/joosbox/compiler/EnvironmentBuilder.scala
+++ joosbox-compiler/src/main/scala/joosbox/compiler/EnvironmentBuilder.scala
@@ -241,12 +241,11 @@ object EnvironmentBuilder {
 
         //  Include all superclasses and implemented interfaces in this scope.
         val linkedScopeReferences: Seq[EnvironmentLookup] = {
-          /*
           (cd.superclass.toSeq ++ cd.interfaces).flatMap {
             case ClassType(tn: TypeName) => Some(TypeNameLookup(tn))
             case InterfaceType(tn: TypeName) => Some(TypeNameLookup(tn))
             case _ => None
-          }*/
+          }
 
           //  For now, just leave this empty because it might break our hierarchy checker.
           Seq.empty[EnvironmentLookup]
