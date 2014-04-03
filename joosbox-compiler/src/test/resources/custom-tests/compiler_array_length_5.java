b1feb4fc913ceb8a37aa9842158326e74fa6814a
Peter Sobot <psobot@gmail.com>
2014-04-03 01:09:45 -0400

    Added array length test.

---
+++ joosbox-compiler/src/test/resources/custom-tests/compiler_array_length_5.java
@@ -0,0 +1,8 @@
+public class compiler_array_length_5 {
+    public compiler_array_length_5() {}
+
+    public static int test() {
+        int[] myArray = new int[5];
+        return myArray.length;
+    }
+}
