49338d50db24bc729f4410773687ec78284f0525
Chris Taylor <cktaylor.se@gmail.com>
2014-04-03 02:08:59 -0400

    fix local test

--- joosbox-compiler/src/test/resources/custom-tests/compiler_array_access_3.java
+++ joosbox-compiler/src/test/resources/custom-tests/compiler_array_access_3.java
@@ -3,7 +3,7 @@ public class compiler_array_access_3 {
     public static int test() {
       int[] x = new int[5];
       x[0] = 1;
-      x[1] = 1;
+      x[1] = 2;
       return x[0] + x[1];
     }
 }
